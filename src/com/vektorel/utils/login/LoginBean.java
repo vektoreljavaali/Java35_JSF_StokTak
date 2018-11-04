1d20df05734d18f87937feede7b1756a395a7535
vektorel <vektorel@uuuu-Bilgisayar>
2018-11-04 09:54:46 +0300

    login fiter

---
+++ src/com/vektorel/utils/login/LoginBean.java
@@ -0,0 +1,12 @@
+package com.vektorel.utils.login;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="loginBean")
+@SessionScoped
+public class LoginBean {
+
+	
+	
+}
---
+++ src/com/vektorel/utils/login/LoginFilter.java
@@ -0,0 +1,5 @@
+package com.vektorel.utils.login;
+
+public class LoginFilter {
+
+}
