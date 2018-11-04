03f58f7541a410fa3de889eb32a4df26366f6e87
vektorel <vektorel@uuuu-Bilgisayar>
2018-11-04 11:49:29 +0300

    .

--- src/com/vektorel/utils/login/LoginBean.java
+++ src/com/vektorel/utils/login/LoginBean.java
@@ -7,6 +7,41 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class LoginBean {
 
+	private String kullaniciadi;
+	private String sifre;
+	public int yetkiid=0;
+	public boolean islogged=false;
+
+	
+	public String girisYap() {
+		
+		if(kullaniciadi.equals("admin") && sifre.equals("123")) {
+			islogged= true;
+			yetkiid = 1;
+			return "AdminPanel/index.jsf";
+		}
+		else
+			return "login.jsf";
+		
+	}
+	
+	public String getKullaniciadi() {
+		return kullaniciadi;
+	}
+
+	public void setKullaniciadi(String kullaniciadi) {
+		this.kullaniciadi = kullaniciadi;
+	}
+
+	public String getSifre() {
+		return sifre;
+	}
+
+	public void setSifre(String sifre) {
+		this.sifre = sifre;
+	}
+	
+	
 	
 	
 }
--- src/com/vektorel/utils/login/LoginFilter.java
+++ src/com/vektorel/utils/login/LoginFilter.java
@@ -1,5 +1,42 @@
 package com.vektorel.utils.login;
 
-public class LoginFilter {
+import java.io.IOException;
 
+import javax.persistence.Id;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginFilter implements Filter{
+
+	@Override
+	public void doFilter(ServletRequest request, 
+						 ServletResponse response, 
+						 FilterChain chain)
+			throws IOException, ServletException {
+		
+	System.out.println("Login filter");
+		
+		LoginBean gelen = (LoginBean) 
+				((HttpServletRequest)request).getSession().getAttribute("LoginBean");
+		
+		
+		if(gelen==null || gelen.yetkiid==1) {
+			
+			String contextPath = ((HttpServletRequest)request).getContextPath();
+            ((HttpServletResponse)response).sendRedirect(contextPath + "/login.jsf");
+            
+		}
+		
+		
+		// bu  kodlama gelen isteðin geldiði gibi 
+		// yönlendirilmesini saðlamaktadýr.
+		 chain.doFilter(request, response);
+	}
+
+	
 }
